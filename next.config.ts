2fce522e9524b9c56cc9cf56dec0897673c0e13c
oz <zaleambo@gmail.com>
2025-05-03 02:19:53 +0300

    removed headers

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,5 @@
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: `
-              default-src 'self';
-              frame-src https://nowpayments.io;
-              script-src 'self' https://nowpayments.io;
-              style-src 'self' 'unsafe-inline';
-              img-src 'self' https://*.nowpayments.io data:;
-              connect-src 'self' https://api.nowpayments.io;
-            `.replace(/\s{2,}/g, ' ').trim()
-          }
-        ]
-      }
-    ]
-  }
+  
 }
 
 module.exports = nextConfig
